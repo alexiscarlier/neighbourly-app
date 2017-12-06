502ef523786683ce89f6527e0b85783e912efd12
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-06 12:16:17 +0000

    Refactor so that App renders Signup and Feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Signup from './Signup';
+import Feed from './Feed';
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Signup />
+        <Feed name="David Halewood" />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Signup from './Signup';
-import Feed from './Feed';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Signup />, document.getElementById('sign-up'));
-ReactDOM.render(<Feed name="David Halewood"/>, document.getElementById('feed'));
 registerServiceWorker();
