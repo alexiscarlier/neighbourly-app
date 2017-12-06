c4b7d6eff632a7bf4732550434667f1351c645da
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-06 11:20:44 +0000

    Create sign up component

---
+++ src/Signup.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Signup extends Component{
+  render() {
+    return (
+      <form>
+        Please enter your name:
+        <input type="text">
+        </input>
+      </form>
+    );
+  }
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Signup from './Signup';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Signup />, document.getElementById('sign-up'));
 registerServiceWorker();
