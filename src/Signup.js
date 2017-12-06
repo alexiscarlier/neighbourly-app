b24376f77070ca79b40b0576fdab67217ef4fa17
Jon Sanders <jonsanders101@aol.com>
2017-12-06 11:55:45 +0000

    Add event listener to Signup

--- src/Signup.js
+++ src/Signup.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+  onClick(){
+    console.log('I was clicked')
+  }
+
   render() {
     return (
+      <div>
       <form>
         Please enter your name:
         <input type="text">
         </input>
       </form>
+      <h2 onClick={this.onClick.bind(this)}>Submit</h2>
+      </div>
     );
   }
 }
