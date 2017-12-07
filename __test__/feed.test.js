e92d83d1192b13343fdddfe913fa328b866bcf32
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-07 14:59:27 +0000

    refactor Feed test

--- __test__/feed.test.js
+++ __test__/feed.test.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import Feed from '../src/Feed'
 
-// describe("<Feed />", () => {
-//     test("receives a name prop", () => {
-//         // failed if shallow() used
-//         const feed = mount(<Feed name="Alfonso"/>);
-//         expect(feed.prop('name')).toEqual("Alfonso");
-//     })
-// })
 
 describe("<Feed />", () => {
     test("it renders welcome message", () => {
