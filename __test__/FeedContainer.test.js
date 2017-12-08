c8aa109d811cb5d11be9544a4641ff8909bce4d9
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-08 11:56:40 +0000

    add test for rendering feed component

---
+++ __test__/FeedContainer.test.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import FeedContainer from '../src/FeedContainer'
+import Feed from '../src/Feed'
+
+
+describe("<FeedContainer />", () => {
+    test("renders <Feed /> if feeds not empty", () => {
+        const renderer = new ShallowRenderer();
+        const feeds = [{address: "Makers Academy"}];
+        renderer.render(<FeedContainer feeds={feeds}/>);
+        const result = renderer.getRenderOutput();
+        expect(result.type).toBe('div');
+        expect(result.props.children).toEqual(<Feed address="Makers Academy" />)
+    })
+})
