ffc66f298cfc18032ceecc572bbad17eb4f2474d
David Halewood <davidnhalewood@hotmail.com>
2017-12-09 23:25:17 +0000

    Adding fields to form for user sign up

--- src/Signup.js
+++ src/Signup.js
@@ -10,15 +10,18 @@ class Signup extends Component {
   onSubmit(e){
     let {address} = this.state;
     this.setState({
-      address: ""
+      address: "",
+      username: "",
+      email: "",
+      password: ""
     });
     this.props.addFeed(address);
     e.preventDefault();
   }
   onChange(e) {
-      this.setState({
-        address: e.target.value
-      });
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
@@ -26,8 +29,10 @@ class Signup extends Component {
       <div>
       <form onSubmit={this.onSubmit.bind(this)}>
         Please enter your address:
-        <input type="text" onChange={this.onChange.bind(this)} value={this.state.address}>
-        </input>
+        <input type="text" name="address" onChange={this.onChange.bind(this)} value={this.state.address} required placeholder="Post Code"/>
+        <input type="text" name="username" onChange={this.onChange.bind(this)} value={this.state.username}required placeholder="Username"/>
+        <input type="text" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required placeholder="Email"/>
+        <input type="text" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required placeholder="Password"/>
         <input type="submit" value="Submit">
         </input>
       </form>
