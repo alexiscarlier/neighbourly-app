fc52827046e3d935d114dc1d00686dd37c718b35
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-10 17:18:38 +0000

    explicitely resets state via onDisconnect

--- src/App.js
+++ src/App.js
@@ -27,7 +27,10 @@ class App extends Component {
     this.socket.emit('feed subscribe')
   }
   onDisconnect() {
-    this.setState({})
+    this.setState({
+      activeFeed: null,
+      feeds: [],
+      connected: false})
   }
 
   onAddFeed(feed) {
