5bdafa03c153703c2d1e7ef1a8f18392a4e3f393
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-10 14:06:57 +0000

    test #onConnect

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import App from '../src/App'
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { WebSocket } from 'mock-socket';
+global.WebSocket = WebSocket;
 
 
+
+const connected = "";
+const activeFeed = null;
 const renderer = new ShallowRenderer();
-renderer.render(<App />);
+renderer.render(<App connected={connected} activeFeed={activeFeed}/>);
 const result = renderer.getRenderOutput();
 
 describe("<App />", () => {
-  test("renders <Signup />", () => {
-    expect(result.type).toBe('div');
+  describe("#render", () => {
+    test("renders a div element", () => {
+      expect(result.type).toBe('div');
+    })   
+  })
+  describe("#onConnect", () => {
+    test("sets connected state to true", () => {
+      const wrapper = mount(<App />);
+      expect(wrapper.state("connected")).toBe(false);      
+      wrapper.instance().onConnect();
+      expect(wrapper.state("connected")).toBe(true);
+    })
   })
 })
