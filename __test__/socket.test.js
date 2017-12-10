3f01b4f846f065073e6b611a70fb6aa3b0f9c73f
Jon Sanders <jonsanders101@aol.com>
2017-12-10 10:31:18 +0000

    Refactor tests

--- __test__/socket.test.js
+++ __test__/socket.test.js
@@ -1,7 +1,5 @@
 import Socket from '../src/socket.js';
 import { Server } from 'mock-socket';
-import { WebSocket as MockWebSocket } from 'mock-socket';
-
 
 const eeMock = {
   emit: jest.fn(),
@@ -15,24 +13,24 @@ const testSocket = new Socket(new WebSocket("ws://localhost:4000"), eeMock);
 describe("Socket", () => {
   describe("#open", () => {
     test("emits 'connect' message when WebSocket connects", (done) => {
-        mockServer.on('connection', server => {
+      mockServer.on('connection', server => {
         mockServer.send(JSON.stringify({name: "message name", data: "message data"}));
       });
-
       setTimeout(() => {
         expect(eeMock.emit.mock.calls[0][0]).toBe('connect');
         mockServer.stop(done);
       }, 100);
     });
   });
-  test("#close", (done) => {
-    testSocket.ws.close();
-    setTimeout(() => {
-      expect(eeMock.emit.mock.calls[2][0]).toBe('disconnect');
-      mockServer.stop(done);
-    }, 1000);
-  });
-
+  describe("#close", () => {
+    test("emits 'disconnect' message when WebSocket closes", (done) => {
+      testSocket.ws.close();
+      setTimeout(() => {
+        expect(eeMock.emit.mock.calls[2][0]).toBe('disconnect');
+        mockServer.stop(done);
+      }, 1000);
+    });
+  })
   describe("#message", () => {
     test("emits message name and data payload", () => {
       expect(eeMock.emit.mock.calls[1][0]).toBe('message name');
--- src/socket.js
+++ src/socket.js
@@ -29,11 +29,9 @@ class Socket {
     this.ws.send(message);
   }
   open(e){
-    console.log("i am the walrus")
     this.ee.emit('connect');
   }
   close(e){
-    console.log('close?');
     this.ee.emit('disconnect');
   }
 }
