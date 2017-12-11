68a19fd59a30f99772f07282535a31463caf401a
Jon Sanders <jonsanders101@aol.com>
2017-12-11 13:45:51 +0000

    Create post component which displays text

---
+++ __test__/Post.test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Post from '../src/Post';
+
+describe("<Post />", () => {
+  test("it renders post title", () => {
+      const renderer = new ShallowRenderer();
+      renderer.render(<Post title="Missing Cats"/>);
+      const result = renderer.getRenderOutput();
+      expect(result.type).toBe('div');
+      expect(result.props.children).toEqual(
+        "Post title: ");
+  });
+});
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render () {
+    return (
+      <div>Post title: </div>
+    );
+  }
+};
+
+export default Post;
