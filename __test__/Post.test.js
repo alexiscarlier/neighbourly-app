981678d47b323a3696ae5e5a09a52dcba6b2c5ea
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-11 13:56:08 +0000

    add text props to post component

--- __test__/Post.test.js
+++ __test__/Post.test.js
@@ -5,10 +5,10 @@ import Post from '../src/Post';
 describe("<Post />", () => {
   test("it renders post title", () => {
       const renderer = new ShallowRenderer();
-      renderer.render(<Post title="Missing Cats"/>);
+      renderer.render(<Post title="Missing Cats" text="Fluffy, cute and missing. $1M reward!"/>);
       const result = renderer.getRenderOutput();
       expect(result.type).toBe('div');
       expect(result.props.children).toEqual([
-        "Post title: ", "Missing Cats"]);
+        "Post title: ", "Missing Cats", " Post: ", "Fluffy, cute and missing. $1M reward!"]);
   });
 });
