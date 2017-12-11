17e5b9bda313f7e449ac9ee1e57805aaabedfc5c
ainsleybc <ab.chang@me.com>
2017-12-11 15:47:46 +0000

    Started google places lookup api functionality

--- src/Signup.js
+++ src/Signup.js
@@ -5,6 +5,8 @@ class Signup extends Component {
   constructor(props){
     super(props);
     this.state = {
+      streetNumber: "",
+      streetName: "",
       postcode: "",
       username: "",
       email: "",
@@ -13,6 +15,8 @@ class Signup extends Component {
   }
   onSubmit(e){
     this.setState({
+      streetNumber: "",
+      streetName: "",
       postcode: "",
       username: "",
       email: "",
@@ -32,16 +36,25 @@ class Signup extends Component {
       <div>
       <form onSubmit={this.onSubmit.bind(this)}>
         <h1>Sign-Up</h1>
-        <input type="text" id="postcode" name="postcode" onChange={this.onChange.bind(this)} value={this.state.postcode} required placeholder="Post Code"/>
+       
+
+          <input id="autocomplete" name="address" onFocus={this.geolocate()} type="text" placeholder="Enter your address"></input>  
+
+          <input id="street_number" onChange={this.onChange.bind(this)} value={this.state.streetNumber} disabled="true"></input>
+          <input id="route" onChange={this.onChange.bind(this)} value={this.state.streetName} disabled="true"></input>
+          <input id="postal_code" onChange={this.onChange.bind(this)} value={this.state.postcode} disabled="true"></input>
+
         <input type="text" id="username" name="username" onChange={this.onChange.bind(this)} value={this.state.username}required placeholder="Username"/>
         <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required placeholder="Email"/>
         <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required placeholder="Password"/>
         <input type="submit" value="Submit">
         </input>
-      </form>
+        </form>
       </div>
     );
   }
 };
+
+  /* <input type="text" id="postcode" name="postcode" onChange={this.onChange.bind(this)} value={this.state.postcode} required placeholder="Post Code" /> */
 
 export default Signup;
