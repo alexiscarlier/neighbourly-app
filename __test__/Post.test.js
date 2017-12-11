b3386caebd3b963505fcb5f440d49f6c7267b516
Jon Sanders <jonsanders101@aol.com>
2017-12-11 14:44:41 +0000

    Add tests for inner divs

--- __test__/Post.test.js
+++ __test__/Post.test.js
@@ -3,11 +3,18 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import Post from '../src/Post';
 
 describe("<Post />", () => {
-  test("it renders post title", () => {
+  test("it renders post title and text", () => {
       const wrapper = mount(<Post title="Missing Cats" text="Fluffy, cute and missing. $1M reward!"/>)
       const result = wrapper.instance();
-      console.log(result);
       expect(result.props.title).toEqual('Missing Cats');
       expect(result.props.text).toEqual("Fluffy, cute and missing. $1M reward!");
+  });
+  test("it renders a div", () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<Post title="Missing Cats" text="Fluffy, cute and missing. $1M reward!"/>);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+    expect(result.props.children[0].type).toBe('div')
+    expect(result.props.children[1].type).toBe('div')
   });
 });
