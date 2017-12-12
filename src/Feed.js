a8fa80d2f6d00cd83eb1c03f2735ddcebf65d7b3
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-12 17:05:50 +0000

    Add onClick to Feed div

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 class Feed extends Component {
+  handleClick() {
+    console.log("This is a serious project david")
+  }
+
   render () {
     return (
-      <div>{this.props.feed.name}</div>
+      <div onClick={this.handleClick}>
+      {this.props.feed.name}
+      </div>
     );
   }
 
