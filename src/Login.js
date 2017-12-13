042e74403e0c008d4d40833302cd015f943bd6b6
David Halewood <davidnhalewood@hotmail.com>
2017-12-13 12:09:20 +0000

    Making jQuery work

--- src/Login.js
+++ src/Login.js
@@ -33,9 +33,15 @@ class Login extends Component {
         <div className="row">
           <div className="col s12 m6 push-m3 card-panel hoverable">
             <form onSubmit={this.onSubmit.bind(this)}>
-              <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required placeholder="Email"/>
+              <div className="input-field col s6">
+                <i className="material-icons prefix">account_circle</i>
+                <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required/>
+                <label className="active" htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s6">
               <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required placeholder="Password"/>
-              <button class="btn waves-effect waves-light" type="submit" value="Log In">Log In<i class="material-icons right">send</i></button>
+              </div>
+              <button className="btn waves-effect waves-light" type="submit" value="Log In">Log In<i className="material-icons right">send</i></button>
             </form>
             <p>or <a href="/signup">create a new account</a></p>
           </div>
