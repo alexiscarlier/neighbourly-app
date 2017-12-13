50ee5623ebccd63050d93d77f6ed23e83c6ee0a2
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-13 07:48:53 +0000

    Add first test

--- __test__/addAddressForm.test.js
+++ __test__/addAddressForm.test.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import AddAddressForm from '../src/AddAddressForm';
 
+const renderer = new ShallowRenderer();
+
 describe("<AddAddressForm />", () => {
   test('renders a form for a user to add an address to a feed', () => {
-
+    const result = renderer.getRenderOutput();
+    expect(result.props.children.type).toBe('form');
   });
 });
