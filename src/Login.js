7de33309e71d2bc26313333aa0e37940e88ab0ac
David Halewood <davidnhalewood@hotmail.com>
2017-12-13 12:16:35 +0000

    Creating container for login input fields

--- src/Login.js
+++ src/Login.js
@@ -33,13 +33,19 @@ class Login extends Component {
         <div className="row">
           <div className="col s12 m6 push-m3 card-panel hoverable">
             <form onSubmit={this.onSubmit.bind(this)}>
-              <div className="input-field col s6">
-                <i className="material-icons prefix">account_circle</i>
-                <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required/>
-                <label className="active" htmlFor="email">Email</label>
-              </div>
-              <div className="input-field col s6">
-              <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required placeholder="Password"/>
+              <div className="row">
+              <div className="col s10 push-s1">
+                  <div className="input-field">
+                    <i className="material-icons prefix">email</i>
+                    <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required/>
+                    <label className="active" htmlFor="email">Email</label>
+                  </div>
+                  <div className="input-field">
+                    <i className="material-icons prefix">lock</i>
+                    <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required/>
+                    <label className="active" htmlFor="email">Password</label>
+                  </div>
+                </div>
               </div>
               <button className="btn waves-effect waves-light" type="submit" value="Log In">Log In<i className="material-icons right">send</i></button>
             </form>
