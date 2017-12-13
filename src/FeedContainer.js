89ef7bbe46651d9e67c0527ffb235db8b5144039
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-13 11:12:02 +0000

    removes validation router

--- src/FeedContainer.js
+++ src/FeedContainer.js
@@ -7,8 +7,8 @@ class FeedContainer extends Component {
   render () {
     return (
       <div>
-        <p>this is the feed</p>
-        {this.props.isConnected ?  (() => {
+        
+         {(() => {
 
         var feeds = this.props.feeds
         var feedList = feeds.map((feed, index) => {
@@ -17,8 +17,8 @@ class FeedContainer extends Component {
 
         return feedList
 
-        })()
-       : <Redirect to='/login' />}
+        })()}
+       
        <FeedForm />
       </div>
     )
