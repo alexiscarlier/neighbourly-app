e653dea72fdbd57b727a52d6a89e1d5a0ad5afd1
David Halewood <davidnhalewood@hotmail.com>
2017-12-13 12:28:41 +0000

    Adding icons as labels

--- src/Login.js
+++ src/Login.js
@@ -30,28 +30,26 @@ class Login extends Component {
     return (
       <div className="containter">
         { this.props.isConnected ? <Redirect to='/feeds' /> :
-        <div className="row">
-          <div className="col s12 m6 push-m3 card-panel hoverable">
-            <form onSubmit={this.onSubmit.bind(this)}>
-              <div className="row">
-              <div className="col s10 push-s1">
-                  <div className="input-field">
-                    <i className="material-icons prefix">email</i>
-                    <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required/>
-                    <label className="active" htmlFor="email">Email</label>
-                  </div>
-                  <div className="input-field">
-                    <i className="material-icons prefix">lock</i>
-                    <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required/>
-                    <label className="active" htmlFor="email">Password</label>
+          <div className="row">
+            <div className="col s12 m6 push-m3 card-panel hoverable">
+              <form onSubmit={this.onSubmit.bind(this)}>
+                <div className="row">
+                  <div className="col s12">
+                    <div className="input-field">
+                      <input type="text" id="email" name="email" onChange={this.onChange.bind(this)} value={this.state.email} required/>
+                      <label className="active" htmlFor="email"><i className="material-icons prefix">email</i></label>
+                    </div>
+                    <div className="input-field">
+                      <input type="password" id="password" name="password" onChange={this.onChange.bind(this)} value={this.state.password}required/>
+                      <label className="active" htmlFor="email"><i className="material-icons prefix">lock</i></label>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <button className="btn waves-effect waves-light" type="submit" value="Log In">Log In<i className="material-icons right">send</i></button>
-            </form>
+                <button className="btn waves-effect waves-light" type="submit" value="Log In">Log In<i className="material-icons right">send</i></button>
+              </form>
             <p>or <a href="/signup">create a new account</a></p>
           </div>
-      </div>}
+        </div>}
       </div>
     )
   }
