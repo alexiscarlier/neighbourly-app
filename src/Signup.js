3b60bba981481ebb630aed9e14836757c02a0e3a
David Halewood <davidnhalewood@hotmail.com>
2017-12-13 15:05:28 +0000

    Adding signup container and button

--- src/Signup.js
+++ src/Signup.js
@@ -38,62 +38,71 @@ class Signup extends Component {
   }
   render() {
     return (
-      <div>
-{ this.props.isConnected ? <Redirect to='/feeds' /> :
-      <form id="signup-form" onSubmit={this.onSubmit.bind(this)} onChange={this.onChange.bind(this)}>
-        <h1>Sign-Up</h1>
-       
-          <input
-            id="autocomplete"
-            name="address"
-           
-            type="text"
-            placeholder="Enter your address"
-          /> 
-          <input
-            id="street_number"
-            name="streetNumber"
-            placeholder="Number"
-            disabled="true"
-          />
-          <input
-            id="route"
-            name="streetName"
-            placeholder="Street"
-            disabled="true"
-          />
-          <input
-            id="postal_code"
-            name="postcode"
-            placeholder="Postcode"
-            disabled="true"
-          />
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={this.state.username}
-            placeholder="Username"
-            required
-          />
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={this.state.email}
-            required
-            placeholder="Email"
-          />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={this.state.password}
-            placeholder="Password"
-            required
-          />
-          <input type="submit" value="Submit" />
-    </form> }
+      <div className ="container">
+          { this.props.isConnected ? <Redirect to='/feeds' /> :
+        <div className="row">
+          <div className="col s12 m6 push-m3 card-panel hoverable login-box">
+            <form id="signup-form" onSubmit={this.onSubmit.bind(this)} onChange={this.onChange.bind(this)}>
+            <div className="row">
+              <div className="col s12">
+                <div className="input-field">
+                  <input
+                    id="autocomplete"
+                    name="address"
+                    
+                    type="text"
+                    placeholder="Enter your address"
+                  /> 
+                  </div>
+              <input
+                id="street_number"
+                name="streetNumber"
+                placeholder="Number"
+                disabled="true"
+              />
+              <input
+                id="route"
+                name="streetName"
+                placeholder="Street"
+                disabled="true"
+              />
+              <input
+                id="postal_code"
+                name="postcode"
+                placeholder="Postcode"
+                disabled="true"
+              />
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={this.state.username}
+                placeholder="Username"
+                required
+              />
+              <input
+                type="text"
+                id="email"
+                name="email"
+                value={this.state.email}
+                required
+                placeholder="Email"
+              />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={this.state.password}
+                placeholder="Password"
+                required
+              />
+              </div>
+              </div>
+              <button className="btn waves-effect waves-light" type="submit" value="Log In">Sign Up<i className="material-icons right">send</i></button>
+            </form>
+            <p>or <a href="/login">log in</a> if you have an account</p>
+          </div>
+        </div>}
       </div>
     );
   }
