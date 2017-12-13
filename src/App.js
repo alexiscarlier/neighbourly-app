2615444cda59c0f03c4366977659e3281c46907b
Lucas Salmins <lucasasalmins@gmail.com>
2017-12-13 20:27:36 +0000

    add header body footer layout

--- src/App.js
+++ src/App.js
@@ -86,38 +86,58 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-              <div>
-                <MainMenu isConnected={this.state.loggedin}/>
-                {this.state.loggedin ? <a href="/" onClick={()=>{this.onDisconnect()}}>Log Off</a> :<hr /> }
+          <div>
+            <header>
+              <div className="container">
+                <div className="row">
+                  <div className="col s12 m9 l9 push-m3 push-l3">
+                    <MainMenu isConnected={this.state.loggedin}/>
+                    {this.state.loggedin ? <a href="/" onClick={()=>{this.onDisconnect()}}>Log Off</a> :<hr /> }
+                    <Route path='/login' render={(props) => (
+                      <Login {...props} key="login" isConnected={this.state.loggedin} userLogin={this.userLogin.bind(this)} />
+                    )}/>
 
-
-                <Route path='/login' render={(props) => (
-                        <Login {...props} key="login" isConnected={this.state.loggedin} userLogin={this.userLogin.bind(this)} />
-                      )}/>
-
-                <Route path="/signup" render={(props) => (
-                        <Signup {...props} key="signup" isConnected={this.state.loggedin} userSignUp={this.userSignUp.bind(this)} />
-                      )}/>
-
-                <Route path="/feeds" render={(props) => (
-                      <div>
+                    <Route path="/signup" render={(props) => (
+                      <Signup {...props} key="signup" isConnected={this.state.loggedin} userSignUp={this.userSignUp.bind(this)} />
+                    )}/>
+                  </div>
+                </div>
+              </div>
+            </header>
+              <Route path="/feeds" render={(props) => (
+                <div >
+                  <main>
+                      <div >
                         <ul id="nav-mobile" className="side-nav fixed" >
-                          <li>
-                            <FeedContainer {...props} key="feedContainer" isConnected={this.state.loggedin} feeds={this.state.feeds} activeFeed={this.state.activeFeed} />                            
-                          </li>
+                          <div className="container">
+                            <li>
+                              <FeedContainer {...props} key="feedContainer" isConnected={this.state.loggedin} feeds={this.state.feeds} activeFeed={this.state.activeFeed} />                            
+                            </li>
+                          </div>
                         </ul>
                         <div className="container">
                           <div className="row">
                             <div className="col s12 m9 l9 push-m3 push-l3">
+                            <h5> THIS IS THE POST CONTAINER </h5>
                               <PostContainer {...props} key="postContainer" posts={this.state.posts}/>
-                              <NewPost {...props} key="newPost" activeFeed={this.state.activeFeed} addPost={this.addPost.bind(this)}/>
                             </div>
                           </div>
                         </div>
                       </div>
-                      )}/>
-              </div>
-            </Router>
+                  </main>
+                  <footer>
+                    <div className="container">
+                      <div className="row">
+                        <div className="col s12 m9 l9 push-m3 push-l3">
+                          <NewPost {...props} key="newPost" activeFeed={this.state.activeFeed} addPost={this.addPost.bind(this)}/>
+                        </div>
+                      </div>
+                    </div>
+                  </footer>
+                </div>
+              )}/>
+          </div>
+        </Router>
       </div>
     );
   }
