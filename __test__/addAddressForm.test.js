656c0be36f554c545c5c99c0a774ee7d4aac7db4
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-13 07:47:28 +0000

    Add test description

--- __test__/addAddressForm.test.js
+++ __test__/addAddressForm.test.js
@@ -1,3 +1,9 @@
 import React, { Component } from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import AddAddressForm from '../src/AddAddressForm';
+
+describe("<AddAddressForm />", () => {
+  test('renders a form for a user to add an address to a feed', () => {
+
+  });
+});
